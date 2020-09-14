cae637d60229bbb15ad7945fac2f9f6dcb94bf8c
ltgrove4fam@gmail.com <ltgrove4fam@gmail.com>
2020-09-14 17:44:53 -0400

    SceneReader coded

--- 3D Engine/SceneReader.cs
+++ 3D Engine/SceneReader.cs
@@ -7,10 +7,15 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.ComponentModel;
 using _3D_Engine;
+using System.Collections.Specialized;
 
 namespace _3D_Engine
 {
     class SceneReader : SceneReaderModule{
-        
+        internal override object run()
+        {
+            scene.addObject(Templates[0], new Vector3(0, 0, 0), 1.0)
+            return (base.run());
+        }
     }
 }
