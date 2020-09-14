7d0b87a8411629d6bdf648b599ae5abad53ee390
Mason Hill <2023hillms@sa.hccsc.us>
2020-09-14 17:55:46 -0400

    Create ReferenceCreators.cs lets begin!

---
+++ 3D Engine/referenceCreators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.ComponentModel;
+using _3D_Engine;
+namespace _3D_Engine
+{
+    class basicReferenceCreator : ReferenceCreatorModule
+    {
+        
+    }
+}
