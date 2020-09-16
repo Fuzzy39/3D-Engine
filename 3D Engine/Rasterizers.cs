5188d4a90badf0b9534f4717b4323ab9f46e3511
Mason Hill <2023hillms@sa.hccsc.us>
2020-09-16 17:33:45 -0400

    Add Rasterizers.cs So it begins...

---
+++ 3D Engine/Rasterizers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _3D_Engine
+{
+    class BasicRasterizer: RasterizerModule
+    {
+
+    }
+}
